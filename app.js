ffceedde6d82b4edebcfb2cc173d58fec6724a98
MalYme <49.132134a@gmail.com>
2024-12-05 23:11:48 +0100

    test

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const btn_start = document.querySelector('.btn_start');
+
+btn_start.addEventListener('click', function() {
+    window.location.href = 'Q1.html'; // Chemin relatif à votre fichier actuel
+   
+    console.log('ACTIVER');
+});
